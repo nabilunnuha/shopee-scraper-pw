fbe8683dce42f11a0397b78c99e475faee414711
kabol <131192836+nabilunnuha@users.noreply.github.com>
2024-06-23 22:44:57 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -7,7 +7,7 @@ REM Mengaktifkan virtual environment (venv)
 call venv\Scripts\activate
 
 REM Menginstal paket-paket yang dibutuhkan
-pip install playwright rich pydantic requests pymongo
+pip install playwright rich pydantic requests pymongo tqdm
 
 REM Menginstal Playwright dan browser yang diperlukan
 playwright install
